2ec3415b1ff6d84143e5bad08ac63aebc49f34f2
Pedro Paz <pedrofelipek@gmail.com>
2025-03-21 19:33:38 -0300

    Update Moto.py
    classe moto

--- Moto.py
+++ Moto.py
@@ -1,2 +1,13 @@
-class Moto
+class Moto:
+    def __init__(self, marca, modelo, ano, cor):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
 
+    def exibir_infos(self):
+        print(f'carro: {self.marca} {self.modelo} {self.ano} {self.cor} ')
+
+moto1 = moto("Honda", "CG 160 Super", 2020, "Preta")
+
+moto1.exibir_infos()
